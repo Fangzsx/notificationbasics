6022936d99198a4d5c643ceee037df624477728c
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-02 20:41:28 +0800

    dismiss persistent notification

--- app/src/main/java/com/fangzsx/notificationbasics/MainActivity.kt
+++ app/src/main/java/com/fangzsx/notificationbasics/MainActivity.kt
@@ -2,12 +2,17 @@ package com.fangzsx.notificationbasics
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.fangzsx.notificationbasics.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,19 +30,35 @@ class MainActivity : AppCompatActivity() {
         createNotificationChannel()
 
 
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                getPendingIntent(0, PendingIntent.FLAG_MUTABLE)
+            } else {
+                getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+            }
+        }
 
         val notificationManager = NotificationManagerCompat.from(this)
 
-        binding.btnSubmitBtn.setOnClickListener {
+        binding.btnSubmit.setOnClickListener {
             val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("This is a test notification")
-                .setContentText(binding.etContent.text.toString())
+                .setContentTitle("Nagru-run to love sa background. Tap to return to app. dun mo sya i-dismiss")
+                .setContentText("Custom Message: ${binding.etContent.text}")
                 .setSmallIcon(R.drawable.ic_sttaus_icon)
                 .setOngoing(true)
+                .setContentIntent(pendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .build()
 
             notificationManager.notify(NOTIFICATION_ID, notification)
+            Snackbar.make(binding.root, "Notification Sent!", Snackbar.LENGTH_SHORT).show()
+        }
+
+        binding.btnDismiss.setOnClickListener {
+            notificationManager.cancel(NOTIFICATION_ID)
+            Snackbar.make(binding.root, "Notification Dismissed!", Snackbar.LENGTH_SHORT).show()
         }
 
     }
