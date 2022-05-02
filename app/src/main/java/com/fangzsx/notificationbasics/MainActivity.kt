a6983de91795138bed226d52e1ae206fef1ef060
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-02 20:10:38 +0800

    show persistent notification

--- app/src/main/java/com/fangzsx/notificationbasics/MainActivity.kt
+++ app/src/main/java/com/fangzsx/notificationbasics/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.fangzsx.notificationbasics
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.fangzsx.notificationbasics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val CHANNEL_ID = "channelID"
+    val CHANNEL_NAME = "channelName"
+
+    val NOTIFICATION_ID = 0
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        createNotificationChannel()
+
+
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        binding.btnSubmitBtn.setOnClickListener {
+            val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("This is a test notification")
+                .setContentText(binding.etContent.text.toString())
+                .setSmallIcon(R.drawable.ic_sttaus_icon)
+                .setOngoing(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build()
+
+            notificationManager.notify(NOTIFICATION_ID, notification)
+        }
+
+    }
+
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
